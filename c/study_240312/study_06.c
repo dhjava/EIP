ce1a32b0fb40e5dc65a0949c39c50de72c0904bf
dhjava <spaghetti0119@gmail.com>
2024-03-12 16:15:41 +0900

    240312 정보처리기사 C언어 공부1

---
+++ c/study_240312/study_06.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(void){
+    int a, b;
+    a = 4 * (1/2);
+    b = a++;
+    printf("%d",b);
+    return 0;
+}
---
+++ c/study_240312/study_11.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    int i = 3;
+    int j = 4;
+    if((++i > j--) && (i++ < --j)) i = i-- + ++j;
+    else j = i-- - --j;
+    printf("%d %d\n", i, j);
+    return 0;
+}
