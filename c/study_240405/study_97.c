a088896ea3a963a2c8b62c1bb9fcd46e743aa145
dhjava <spaghetti0119@gmail.com>
2024-04-05 19:17:26 +0900

    20240405 c언어 학습

---
+++ c/study_240405/study_97.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int a = 10;
+int b = 20;
+int c = 30;
+void func(void){
+    static int a = 100;
+    int b = 200;
+    a++;
+    b++;
+    c = a;
+}
+int main(void){
+    func();
+    func();
+    printf("a = %d, b = %d, c = %d\n", a, b, c);
+    return 0;
+}
